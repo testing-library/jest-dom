c7e8b2981c79fc25174a4fe7d6fb11a7d588b4f1
Ernesto Garcia <gnapse@gmail.com>
2018-05-17 22:34:48 -0400

    feat: toBeVisible custom matcher

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import {toHaveTextContent} from './to-have-text-content'
 import {toHaveAttribute} from './to-have-attribute'
 import {toHaveClass} from './to-have-class'
 import {toHaveStyle} from './to-have-style'
+import {toBeVisible} from './to-be-visible'
 
 export {
   toBeInTheDOM,
@@ -10,4 +11,5 @@ export {
   toHaveAttribute,
   toHaveClass,
   toHaveStyle,
+  toBeVisible,
 }
