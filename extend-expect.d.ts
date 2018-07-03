ee4d46f3c150749b382a9ed915983b2f9eeceb4e
Seth MacPherson <smacpherson64@gmail.com>
2018-07-03 18:32:36 -0500

    #3: Updated types to match functions

--- extend-expect.d.ts
+++ extend-expect.d.ts
@@ -1,9 +1,12 @@
+import { toContainElement } from "./src";
+
 declare namespace jest {
   interface Matchers<R> {
     toHaveAttribute: (attr: string, value?: string) => R
     toHaveTextContent: (text: string) => R
     toHaveClass: (className: string) => R
-    toBeInTheDOM: () => R
+    toBeInTheDOM: (container: HTMLElement) => R
+    toContainElement: (element: HTMLElement) => R
     toHaveStyle: (css: string) => R
   }
 }
