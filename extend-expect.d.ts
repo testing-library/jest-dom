addebadc0738a99d084bd8640402d4c391e22658
John Gozde <john@gozde.ca>
2018-07-09 09:33:17 -0600

    fix(TypeScript): Convert matchers to instance methods (#30)
    * Convert matcher typings to instance methods
    
    * Allow RegExp in toHaveTextContent

--- extend-expect.d.ts
+++ extend-expect.d.ts
@@ -1,12 +1,12 @@
 declare namespace jest {
   interface Matchers<R> {
-    toBeInTheDOM: (container?: HTMLElement) => R
-    toBeVisible: () => R
-    toBeEmpty: () => R
-    toContainElement: (element: HTMLElement) => R
-    toHaveAttribute: (attr: string, value?: string) => R
-    toHaveClass: (className: string) => R
-    toHaveStyle: (css: string) => R
-    toHaveTextContent: (text: string) => R
+    toBeInTheDOM(container?: HTMLElement): R
+    toBeVisible(): R
+    toBeEmpty(): R
+    toContainElement(element: HTMLElement): R
+    toHaveAttribute(attr: string, value?: string): R
+    toHaveClass(className: string): R
+    toHaveStyle(css: string): R
+    toHaveTextContent(text: string | RegExp): R
   }
 }
