ab0ed2f45f031c6337cc3ca57aa3affc89eaea13
John Gozde <john@gozde.ca>
2018-07-11 14:30:48 -0600

    Allow SVGElement in toContainElement and toBeInTheDOM

--- extend-expect.d.ts
+++ extend-expect.d.ts
@@ -1,10 +1,10 @@
 declare namespace jest {
   interface Matchers<R> {
-    toBeInTheDOM(container?: HTMLElement): R
+    toBeInTheDOM(container?: HTMLElement | SVGElement): R
     toBeVisible(): R
     toBeEmpty(): R
     toBeDisabled(): R
-    toContainElement(element: HTMLElement): R
+    toContainElement(element: HTMLElement | SVGElement): R
     toHaveAttribute(attr: string, value?: string): R
     toHaveClass(...classNames: string[]): R
     toHaveStyle(css: string): R
