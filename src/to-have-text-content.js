a6f7e5992b04758f4ea4f72f9a6734f27185128c
Seth MacPherson <smacpherson64@gmail.com>
2018-09-07 14:44:11 -0500

    #9: Normalized whitespace using dom-testing-library

--- src/to-have-text-content.js
+++ src/to-have-text-content.js
@@ -1,9 +1,11 @@
 import {matcherHint} from 'jest-matcher-utils'
+import {getNodeText} from 'dom-testing-library'
 import {checkHtmlElement, getMessage, matches} from './utils'
 
 export function toHaveTextContent(htmlElement, checkWith) {
   checkHtmlElement(htmlElement, toHaveTextContent, this)
-  const textContent = htmlElement.textContent
+  const textContent = getNodeText(htmlElement).replace(/\s+/g, ' ')
+
   return {
     pass: matches(textContent, htmlElement, checkWith),
     message: () => {
