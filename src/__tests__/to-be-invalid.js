0a80343343d29f31702cc97c867d56e87a268fd4
Damien Senger <damien@raccoon.studio>
2019-05-23 21:04:57 +0200

    Refactor the tests for .toBeInvalid

--- src/__tests__/to-be-invalid.js
+++ src/__tests__/to-be-invalid.js
@@ -1,6 +1,34 @@
 import {JSDOM} from 'jsdom'
 import {render} from './helpers/test-utils'
 
+/*
+ * This function is being used to test if `.toBeInvalid` and `.toBeValid`
+ * are correctly triggered by the DOM Node method `.checkValidity()`, part
+ * of the Web API.
+ *
+ * For this check, we are using the `jsdom` library to return a DOM Node
+ * sending the good information to our test.
+ *
+ * We are using this library because without it `.checkValidity()` returns
+ * always `true` when using `yarn test` in a terminal.
+ *
+ * Please consult the PR 110 to get more information:
+ * https://github.com/testing-library/jest-dom/pull/110
+ *
+ * @link https://github.com/testing-library/jest-dom/pull/110
+ * @link https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5/Constraint_validation
+ * @link https://github.com/jsdom/jsdom
+ */
+function getDOMInput(htmlString) {
+  const dom = new JSDOM(htmlString)
+
+  return dom.window.document.querySelector('input')
+}
+
+const invalidInputNode = getDOMInput(
+  `<input pattern="[a-z]{1,3}" value="test+">`,
+)
+
 test('.toBeInvalid', () => {
   const {queryByTestId} = render(`
     <div>
@@ -11,13 +39,11 @@ test('.toBeInvalid', () => {
     </div>
     `)
 
-  const dom = new JSDOM(`<input pattern="[a-z]{1,3}" value="test+">`)
-
   expect(queryByTestId('no-aria-invalid')).not.toBeInvalid()
   expect(queryByTestId('aria-invalid')).toBeInvalid()
   expect(queryByTestId('aria-invalid-value')).toBeInvalid()
   expect(queryByTestId('aria-invalid-false')).not.toBeInvalid()
-  expect(dom.window.document.querySelector('input')).toBeInvalid()
+  expect(invalidInputNode).toBeInvalid()
 
   // negative test cases wrapped in throwError assertions for coverage.
   expect(() =>
@@ -32,9 +58,7 @@ test('.toBeInvalid', () => {
   expect(() =>
     expect(queryByTestId('aria-invalid-false')).toBeInvalid(),
   ).toThrowError()
-  expect(() =>
-    expect(dom.window.document.querySelector('input')).not.toBeInvalid(),
-  ).toThrowError()
+  expect(() => expect(invalidInputNode).not.toBeInvalid()).toThrowError()
 })
 
 test('.toBeValid', () => {
@@ -47,13 +71,11 @@ test('.toBeValid', () => {
     </div>
     `)
 
-  const dom = new JSDOM(`<input pattern="[a-z]{1,3}" value="test+">`)
-
   expect(queryByTestId('no-aria-invalid')).toBeValid()
   expect(queryByTestId('aria-invalid')).not.toBeValid()
   expect(queryByTestId('aria-invalid-value')).not.toBeValid()
   expect(queryByTestId('aria-invalid-false')).toBeValid()
-  expect(dom.window.document.querySelector('input')).not.toBeValid()
+  expect(invalidInputNode).not.toBeValid()
 
   // negative test cases wrapped in throwError assertions for coverage.
   expect(() =>
@@ -66,7 +88,5 @@ test('.toBeValid', () => {
   expect(() =>
     expect(queryByTestId('aria-invalid-false')).not.toBeValid(),
   ).toThrowError()
-  expect(() =>
-    expect(dom.window.document.querySelector('input')).toBeValid(),
-  ).toThrowError()
+  expect(() => expect(invalidInputNode).toBeValid()).toThrowError()
 })
