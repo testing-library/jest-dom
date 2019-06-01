3eb11eef897cae7c1497e104cd9ef16367ce2ba4
Damien Senger <damien@raccoon.studio>
2019-06-01 12:32:02 +0200

    Add markup example to code documentation

--- src/to-be-labelled.js
+++ src/to-be-labelled.js
@@ -17,6 +17,9 @@ const TAGS_WITH_TEXT_CONTENT = ['a', 'button', 'object']
 
 /*
  * Checks if the element is labelled by `aria-label`
+ *
+ * e.g. `<button aria-label="Add new item"></button>`
+ *
  * @link https://www.w3.org/TR/WCAG20-TECHS/ARIA6.html
  */
 function isHavingARIALabel(element) {
@@ -25,6 +28,9 @@ function isHavingARIALabel(element) {
 
 /*
  * Checks if the element is labelled by `aria-labelledby`
+ *
+ * e.g. `<svg aria-labelledby="refId"></svg>`
+ *
  * @link https://www.w3.org/TR/WCAG20-TECHS/ARIA10.html
  */
 function isHavingARIALabelledBy(element) {
@@ -33,6 +39,9 @@ function isHavingARIALabelledBy(element) {
 
 /*
  * Checks if meaningful images are having an alt attribute
+ *
+ * e.g. `<img src="" alt="Text alternative" />`
+ *
  * @link https://www.w3.org/TR/WCAG20-TECHS/H37.html
  */
 function isHavingMeaningfulAlt(element) {
@@ -45,6 +54,9 @@ function isHavingMeaningfulAlt(element) {
 
 /*
  * Checks if there is a title tag as a child
+ *
+ * e.g. `<svg><title>Title for this non-text content</title></svg>`
+ *
  * @link https://www.w3.org/TR/WCAG20-TECHS/H65.html
  */
 function isHavingMeaningfulTitle(element) {
@@ -54,6 +66,11 @@ function isHavingMeaningfulTitle(element) {
   )
 }
 
+/*
+ * Checks if there is a title attribute
+ *
+ * e.g. `<abbr title="United Nations">UN</abbr>`
+ */
 function isHavingMeaningfulTitleAttribute(element) {
   return (
     TAGS_WITH_TITLE_ATTR.includes(getTag(element)) &&
@@ -64,6 +81,9 @@ function isHavingMeaningfulTitleAttribute(element) {
 
 /*
  * Checks if there is an adjacent image and text
+ *
+ * e.g. `<button><img src="" alt="">Add new item</button>`
+ *
  * @link https://www.w3.org/TR/WCAG20-TECHS/G95.html
  */
 function isHavingASiblingLabel(element) {
@@ -75,7 +95,8 @@ function isHavingASiblingLabel(element) {
 }
 
 /*
- * Checks if there is an adjacent image and text
+ * Checks if the image is having a meaningful content with with ARIA, an alt attribute or a title attribute
+ *
  * @link https://www.w3.org/TR/WCAG20-TECHS/H2.html
  */
 function isHavingAContentText(element) {
