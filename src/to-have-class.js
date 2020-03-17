a8c57d6948b44b0f291f659b86995f30e8280e39
root <root@LAPTOP-VOQ3N65A.localdomain>
2020-03-17 23:13:26 +0100

    feat: add exact mode option for toHaveClass (#176)

--- src/to-have-class.js
+++ src/to-have-class.js
@@ -1,6 +1,20 @@
 import {matcherHint, printExpected} from 'jest-matcher-utils'
 import {checkHtmlElement, getMessage} from './utils'
 
+function getExpectedClassNamesAndOptions(params) {
+  const lastParam = params.pop()
+  let expectedClassNames, options
+
+  if (typeof lastParam === 'object') {
+    expectedClassNames = params
+    options = lastParam
+  } else {
+    expectedClassNames = params.concat(lastParam)
+    options = {}
+  }
+  return {expectedClassNames, options}
+}
+
 function splitClassNames(str) {
   if (!str) {
     return []
@@ -12,13 +26,31 @@ function isSubset(subset, superset) {
   return subset.every(item => superset.includes(item))
 }
 
-export function toHaveClass(htmlElement, ...expectedClassNames) {
+export function toHaveClass(htmlElement, ...params) {
   checkHtmlElement(htmlElement, toHaveClass, this)
+  const {expectedClassNames, options} = getExpectedClassNamesAndOptions(params)
+
   const received = splitClassNames(htmlElement.getAttribute('class'))
   const expected = expectedClassNames.reduce(
     (acc, className) => acc.concat(splitClassNames(className)),
     [],
   )
+
+  if (options.exact) {
+    return {
+      pass: isSubset(expected, received) && expected.length === received.length,
+      message: () => {
+        const to = this.isNot ? 'not to' : 'to'
+        return getMessage(
+          `Expected the element ${to} have EXACTLY defined classes`,
+          expected.join(' '),
+          'Received',
+          received.join(' '),
+        )
+      },
+    }
+  }
+
   return expected.length > 0
     ? {
         pass: isSubset(expected, received),
