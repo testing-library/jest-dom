3cf97e2864c2c20671f3ae681619b5641f12584c
Matej Snuderl <ematej.snuderl@gmail.com>
2020-05-07 19:59:29 +0200

    chore(tests): 100% test coverage

--- src/__tests__/helpers/test-utils.ts
+++ src/__tests__/helpers/test-utils.ts
@@ -1,5 +1,4 @@
 import document from './document'
-import * as extensions from '../../matchers'
 
 function render(html: string) {
   const container = document.createElement('div')
@@ -7,14 +6,6 @@ function render(html: string) {
   const queryByTestId = (testId: string) =>
     container.querySelector(`[data-testid="${testId}"]`)
   return {container, queryByTestId}
-}
-
-type Extensions = typeof extensions
-
-declare global {
-  namespace jest {
-    interface Matchers<R> extends Extensions {}
-  }
 }
 
 export {render}
