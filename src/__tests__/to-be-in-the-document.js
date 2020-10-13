e0c7ad5944bbd78121842644dcc92e21176d10b9
Geoff Rich <geoff.rich@alaskaair.com>
2020-10-13 08:22:19 -0700

    fix: treat shadow DOM elements as in the document

--- src/__tests__/to-be-in-the-document.js
+++ src/__tests__/to-be-in-the-document.js
@@ -2,12 +2,29 @@ import {HtmlElementTypeError} from '../utils'
 import document from './helpers/document'
 
 test('.toBeInTheDocument', () => {
+  const window = document.defaultView
+
+  window.customElements.define(
+    'custom-element',
+    class extends window.HTMLElement {
+      constructor() {
+        super()
+        this.attachShadow({mode: 'open'}).innerHTML =
+          '<div data-testid="custom-element-child"></div>'
+      }
+    },
+  )
+
   document.body.innerHTML = `
     <span data-testid="html-element"><span>Html Element</span></span>
-    <svg data-testid="svg-element"></svg>`
+    <svg data-testid="svg-element"></svg>
+    <custom-element data-testid="custom-element"></custom-element>`
 
   const htmlElement = document.querySelector('[data-testid="html-element"]')
   const svgElement = document.querySelector('[data-testid="svg-element"]')
+  const customElementChild = document
+    .querySelector('[data-testid="custom-element"]')
+    .shadowRoot.querySelector('[data-testid="custom-element-child"]')
   const detachedElement = document.createElement('div')
   const fakeElement = {thisIsNot: 'an html element'}
   const undefinedElement = undefined
@@ -15,6 +32,7 @@ test('.toBeInTheDocument', () => {
 
   expect(htmlElement).toBeInTheDocument()
   expect(svgElement).toBeInTheDocument()
+  expect(customElementChild).toBeInTheDocument()
   expect(detachedElement).not.toBeInTheDocument()
   expect(nullElement).not.toBeInTheDocument()
 
