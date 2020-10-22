d4424f151a32dd57ef97b11083aa9fe68fe1a670
Richard Laffers <rlaffers@gmail.com>
2020-10-22 19:22:22 +0200

    Add support for aria-disabled attribute

--- src/__tests__/to-be-disabled.js
+++ src/__tests__/to-be-disabled.js
@@ -42,7 +42,11 @@ test('.toBeDisabled', () => {
   expect(queryByTestId('fieldset-element')).toBeDisabled()
   expect(queryByTestId('fieldset-child-element')).toBeDisabled()
 
-  expect(queryByTestId('div-element')).not.toBeDisabled()
+  expect(() =>
+    expect(queryByTestId('div-element')).not.toBeDisabled(),
+  ).toThrowError(
+    /only focusable form elements or elements with role=.* can be used/,
+  )
   expect(queryByTestId('div-child-element')).not.toBeDisabled()
 
   expect(queryByTestId('nested-form-element')).toBeDisabled()
@@ -50,12 +54,116 @@ test('.toBeDisabled', () => {
   expect(queryByTestId('deep-optgroup-element')).toBeDisabled()
   expect(queryByTestId('deep-option-element')).toBeDisabled()
 
-  expect(queryByTestId('a-element')).not.toBeDisabled()
-  expect(queryByTestId('deep-a-element')).not.toBeDisabled()
-  expect(() => expect(queryByTestId('a-element')).toBeDisabled()).toThrowError()
+  expect(() => expect(queryByTestId('a-element')).toBeDisabled()).toThrowError(
+    /only focusable form elements or elements with role=.* can be used/,
+  )
   expect(() =>
     expect(queryByTestId('deep-a-element')).toBeDisabled(),
-  ).toThrowError()
+  ).toThrowError(
+    /only focusable form elements or elements with role=.* can be used/,
+  )
+})
+
+const roles = [
+  'checkbox',
+  'radio',
+  'switch',
+  'button',
+  'combobox',
+  'select',
+  'option',
+  'textbox',
+  'input',
+]
+
+describe('.toBeDisabled on element with ARIA role', () => {
+  test('handles element with supported role', () => {
+    roles.forEach(role => {
+      const {queryByTestId} = render(`
+          <div role="${role}" aria-disabled="true" data-testid="aria-${role}-disabled"></div>
+          <div role="${role}" aria-disabled="false" data-testid="aria-${role}-enabled"></div>
+      `)
+      expect(queryByTestId(`aria-${role}-disabled`)).toBeDisabled()
+      expect(queryByTestId(`aria-${role}-enabled`)).not.toBeDisabled()
+    })
+  })
+
+  test('throws when element with supported role is disabled but expected not to be', () => {
+    roles.forEach(role => {
+      const {queryByTestId} = render(`
+          <div role="${role}" aria-disabled="true" data-testid="aria-${role}-disabled"></div>
+      `)
+      expect(() =>
+        expect(queryByTestId(`aria-${role}-disabled`)).not.toBeDisabled(),
+      ).toThrowError()
+    })
+  })
+
+  test('throws when element with supported role is not disabled but expected to be', () => {
+    roles.forEach(role => {
+      const {queryByTestId} = render(`
+          <div role="${role}" aria-disabled="false" data-testid="aria-${role}-enabled"></div>
+      `)
+      expect(() =>
+        expect(queryByTestId(`aria-${role}-enabled`)).toBeDisabled(),
+      ).toThrowError()
+    })
+  })
+
+  test('handles inheritance of the disabled state from a parent with supported role', () => {
+    const {queryByTestId} = render(`
+      <div>
+        <fieldset disabled={true} data-testid="fieldset-element">
+          <div role="button" data-testid="fieldset-child-element">x</div>
+        </fieldset>
+
+        <div role="select" aria-disabled="true" data-testid="div-element">
+          <div role="option" data-testid="div-child-element">x</div>
+        </div>
+
+        <div role="select" aria-disabled="false" data-testid="div-element-enabled">
+          <div role="option" data-testid="div-child-element-enabled">x</div>
+        </div>
+
+        <div role="group" aria-disabled="true">
+          <div>
+            <button data-testid="nested-form-element">x</button>
+
+            <select data-testid="deep-select-element">
+              <optgroup data-testid="deep-optgroup-element">
+                <option data-testid="deep-option-element">x</option>
+              </optgroup>
+            </select>
+          </div>
+          <a href="http://github.com" data-testid="deep-a-element">x</a>
+        </div>
+
+        <a href="http://github.com" aria-disabled="true" data-testid="a-element">x</a>
+      </div>
+      `)
+
+    expect(queryByTestId('fieldset-element')).toBeDisabled()
+    expect(queryByTestId('fieldset-child-element')).toBeDisabled()
+    expect(queryByTestId('div-element')).toBeDisabled()
+    expect(queryByTestId('div-child-element')).toBeDisabled()
+    expect(queryByTestId('div-element-enabled')).not.toBeDisabled()
+    expect(queryByTestId('div-child-element-enabled')).not.toBeDisabled()
+    expect(queryByTestId('nested-form-element')).toBeDisabled()
+    expect(queryByTestId('deep-select-element')).toBeDisabled()
+    expect(queryByTestId('deep-optgroup-element')).toBeDisabled()
+    expect(queryByTestId('deep-option-element')).toBeDisabled()
+
+    expect(() =>
+      expect(queryByTestId('a-element')).toBeDisabled(),
+    ).toThrowError(
+      /only focusable form elements or elements with role=.* can be used/,
+    )
+    expect(() =>
+      expect(queryByTestId('deep-a-element')).toBeDisabled(),
+    ).toThrowError(
+      /only focusable form elements or elements with role=.* can be used/,
+    )
+  })
 })
 
 test('.toBeDisabled fieldset>legend', () => {
@@ -159,7 +267,9 @@ test('.toBeEnabled', () => {
     expect(queryByTestId('fieldset-child-element')).toBeEnabled()
   }).toThrowError()
 
-  expect(queryByTestId('div-element')).toBeEnabled()
+  expect(() => expect(queryByTestId('div-element')).toBeEnabled()).toThrowError(
+    /only focusable form elements or elements with role=.* can be used/,
+  )
   expect(queryByTestId('div-child-element')).toBeEnabled()
 
   expect(() => {
@@ -175,14 +285,113 @@ test('.toBeEnabled', () => {
     expect(queryByTestId('deep-option-element')).toBeEnabled()
   }).toThrowError()
 
-  expect(queryByTestId('a-element')).toBeEnabled()
+  expect(() => expect(queryByTestId('a-element')).toBeEnabled()).toThrowError(
+    /only focusable form elements or elements with role=.* can be used/,
+  )
   expect(() =>
     expect(queryByTestId('a-element')).not.toBeEnabled(),
-  ).toThrowError()
-  expect(queryByTestId('deep-a-element')).toBeEnabled()
+  ).toThrowError(
+    /only focusable form elements or elements with role=.* can be used/,
+  )
+
+  expect(() =>
+    expect(queryByTestId('deep-a-element')).toBeEnabled(),
+  ).toThrowError(
+    /only focusable form elements or elements with role=.* can be used/,
+  )
   expect(() =>
     expect(queryByTestId('deep-a-element')).not.toBeEnabled(),
-  ).toThrowError()
+  ).toThrowError(
+    /only focusable form elements or elements with role=.* can be used/,
+  )
+})
+
+describe('.toBeEnabled on element with ARIA role', () => {
+  test('handles element with supported role', () => {
+    roles.forEach(role => {
+      const {queryByTestId} = render(`
+          <div role="${role}" aria-disabled="true" data-testid="aria-${role}-disabled"></div>
+          <div role="${role}" aria-disabled="false" data-testid="aria-${role}-enabled"></div>
+      `)
+      expect(queryByTestId(`aria-${role}-disabled`)).not.toBeEnabled()
+      expect(queryByTestId(`aria-${role}-enabled`)).toBeEnabled()
+    })
+  })
+
+  test('throws when element with supported role is enabled but expected not to be', () => {
+    roles.forEach(role => {
+      const {queryByTestId} = render(`
+          <div role="${role}" aria-disabled="false" data-testid="aria-${role}-enabled"></div>
+      `)
+      expect(() =>
+        expect(queryByTestId(`aria-${role}-enabled`)).not.toBeEnabled(),
+      ).toThrowError()
+    })
+  })
+
+  test('throws when element with supported role is not enabled but expected to be', () => {
+    roles.forEach(role => {
+      const {queryByTestId} = render(`
+          <div role="${role}" aria-disabled="true" data-testid="aria-${role}-disabled"></div>
+      `)
+      expect(() =>
+        expect(queryByTestId(`aria-${role}-disabled`)).toBeEnabled(),
+      ).toThrowError()
+    })
+  })
+
+  test('handles inheritance of the disabled state from a parent with supported role', () => {
+    const {queryByTestId} = render(`
+      <div>
+        <fieldset disabled={true} data-testid="fieldset-element">
+          <div role="button" data-testid="fieldset-child-element">x</div>
+        </fieldset>
+
+        <div role="select" aria-disabled="true" data-testid="div-element">
+          <div role="option" data-testid="div-child-element">x</div>
+        </div>
+
+        <div role="select" aria-disabled="false" data-testid="div-element-enabled">
+          <div role="option" data-testid="div-child-element-enabled">x</div>
+        </div>
+
+        <div role="group" aria-disabled="true">
+          <div>
+            <button data-testid="nested-form-element">x</button>
+
+            <select data-testid="deep-select-element">
+              <optgroup data-testid="deep-optgroup-element">
+                <option data-testid="deep-option-element">x</option>
+              </optgroup>
+            </select>
+          </div>
+          <a href="http://github.com" data-testid="deep-a-element">x</a>
+        </div>
+
+        <a href="http://github.com" aria-disabled="true" data-testid="a-element">x</a>
+      </div>
+      `)
+
+    expect(queryByTestId('fieldset-element')).not.toBeEnabled()
+    expect(queryByTestId('fieldset-child-element')).not.toBeEnabled()
+    expect(queryByTestId('div-element')).not.toBeEnabled()
+    expect(queryByTestId('div-child-element')).not.toBeEnabled()
+    expect(queryByTestId('div-element-enabled')).toBeEnabled()
+    expect(queryByTestId('div-child-element-enabled')).toBeEnabled()
+    expect(queryByTestId('nested-form-element')).not.toBeEnabled()
+    expect(queryByTestId('deep-select-element')).not.toBeEnabled()
+    expect(queryByTestId('deep-optgroup-element')).not.toBeEnabled()
+    expect(queryByTestId('deep-option-element')).not.toBeEnabled()
+
+    expect(() => expect(queryByTestId('a-element')).toBeEnabled()).toThrowError(
+      /only focusable form elements or elements with role=.* can be used/,
+    )
+    expect(() =>
+      expect(queryByTestId('deep-a-element')).toBeEnabled(),
+    ).toThrowError(
+      /only focusable form elements or elements with role=.* can be used/,
+    )
+  })
 })
 
 test('.toBeEnabled fieldset>legend', () => {
--- src/to-be-disabled.js
+++ src/to-be-disabled.js
@@ -1,4 +1,5 @@
-import {checkHtmlElement, getTag} from './utils'
+import {roles} from 'aria-query'
+import {checkHtmlElement, getTag, toSentence} from './utils'
 
 // form elements that support 'disabled'
 const FORM_TAGS = [
@@ -36,12 +37,37 @@ function isElementDisabledByParent(element, parent) {
   )
 }
 
+function roleSupportsDisabled(role) {
+  return roles.get(role)?.props['aria-disabled'] !== undefined
+}
+
+function supportedRoles() {
+  return Array.from(roles.keys()).filter(roleSupportsDisabled)
+}
+
+function supportedRolesSentence() {
+  return toSentence(
+    supportedRoles().map(role => `role="${role}"`),
+    {lastWordConnector: ' or '},
+  )
+}
+
+function isAriaDisableable(element) {
+  return roleSupportsDisabled(element.getAttribute('role'))
+}
+
 function canElementBeDisabled(element) {
   return FORM_TAGS.includes(getTag(element))
 }
 
 function isElementDisabled(element) {
-  return canElementBeDisabled(element) && element.hasAttribute('disabled')
+  if (canElementBeDisabled(element)) {
+    return element.hasAttribute('disabled')
+  } else if (isAriaDisableable(element)) {
+    return element.getAttribute('aria-disabled') === 'true'
+  } else {
+    return false
+  }
 }
 
 function isAncestorDisabled(element) {
@@ -53,14 +79,19 @@ function isAncestorDisabled(element) {
 }
 
 function isElementOrAncestorDisabled(element) {
-  return (
-    canElementBeDisabled(element) &&
-    (isElementDisabled(element) || isAncestorDisabled(element))
-  )
+  return isElementDisabled(element) || isAncestorDisabled(element)
 }
 
 export function toBeDisabled(element) {
   checkHtmlElement(element, toBeDisabled, this)
+
+  if (!canElementBeDisabled(element) && !isAriaDisableable(element)) {
+    return {
+      pass: this.isNot,
+      message: () =>
+        `only focusable form elements or elements with ${supportedRolesSentence()} can be used with .toBeDisabled().`,
+    }
+  }
 
   const isDisabled = isElementOrAncestorDisabled(element)
 
@@ -84,6 +115,14 @@ export function toBeDisabled(element) {
 
 export function toBeEnabled(element) {
   checkHtmlElement(element, toBeEnabled, this)
+
+  if (!canElementBeDisabled(element) && !isAriaDisableable(element)) {
+    return {
+      pass: this.isNot,
+      message: () =>
+        `only focusable form elements or elements with ${supportedRolesSentence()} can be used with .toBeEnabled().`,
+    }
+  }
 
   const isEnabled = !isElementOrAncestorDisabled(element)
 
