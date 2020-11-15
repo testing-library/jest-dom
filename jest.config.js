42975ac49e1e5aefe0dfacd13eebf1430071eae3
Nick McCurdy <nick@nickmccurdy.com>
2020-11-15 00:24:04 -0500

    chore: fix jest-watch-select-projects

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,11 @@ const config = require('kcd-scripts/jest')
 
 module.exports = {
   ...config,
-  projects: [
+  watchPlugins: [
+    ...config.watchPlugins,
     require.resolve('jest-watch-select-projects'),
+  ],
+  projects: [
     require.resolve('./tests/jest.config.dom'),
     require.resolve('./tests/jest.config.node'),
   ],
