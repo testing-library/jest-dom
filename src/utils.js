2594583dc60e9a5f4afd8f6425e65073ada95735
Jean-Philippe Zolesio <zolesio@adobe.com>
2022-08-03 20:19:14 -0700

    Upgrade @adobe/css-tools to version 4.0.1 (umd build should be usable in nodejs and web browser environment)

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import redent from 'redent'
-import {parse} from '@adobe/css-tools/dist/cjs/cssTools'
 import isEqual from 'lodash/isEqual'
+import {parse} from '@adobe/css-tools'
 
 class GenericTypeError extends Error {
   constructor(expectedString, received, matcherFn, context) {
