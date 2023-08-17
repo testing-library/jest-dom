d2b130d543f112e0118005ea573cd4a91d6d508d
John Gozde <john@gozde.ca>
2023-08-17 10:58:18 -0600

    Fix all AsymmetricMatcher interfaces

--- types/jest-globals.d.ts
+++ types/jest-globals.d.ts
@@ -4,5 +4,8 @@ import {type TestingLibraryMatchers} from './matchers'
 export {}
 declare module '@jest/expect' {
   export interface Matchers<R extends void | Promise<void>>
-    extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
+    extends TestingLibraryMatchers<
+      ReturnType<typeof expect.stringContaining>,
+      R
+    > {}
 }
