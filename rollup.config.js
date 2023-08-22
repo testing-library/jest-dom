17700fbab76bd0d72f8f30e6aa44d25b04be8d56
Ian VanSchooten <ian.vanschooten@gmail.com>
2023-08-22 10:32:31 -0400

    Convert cjs to esm if needed

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 const del = require('rollup-plugin-delete')
+const commonjs = require('@rollup/plugin-commonjs')
 
 const entries = [
   './src/index.js',
@@ -26,6 +27,6 @@ module.exports = [
     ],
     external: id =>
       !id.startsWith('\0') && !id.startsWith('.') && !id.startsWith('/'),
-    plugins: [del({targets: 'dist/*'})],
+    plugins: [del({targets: 'dist/*'}), commonjs()],
   },
 ]
