805ac1c6fe7f1a0c6664bd23fdc1d436e98c135e
nyaapass <13141973+nyaapass@users.noreply.github.com>
2024-03-11 23:51:51 +0900

    fix: proper vitest ts support (#515)

--- types/vitest.d.ts
+++ types/vitest.d.ts
@@ -1,11 +1,16 @@
-import {type expect} from 'vitest'
+/* eslint-disable @typescript-eslint/no-shadow */
+
+import {type Assertion, type AsymmetricMatchersContaining} from 'vitest'
 import {type TestingLibraryMatchers} from './matchers'
 
 export {}
-declare module '@vitest/expect' {
-  interface JestAssertion<T = any>
+
+// https://vitest.dev/guide/extending-matchers.html
+declare module 'vitest' {
+  interface Assertion<T = any>
     extends TestingLibraryMatchers<
       ReturnType<typeof expect.stringContaining>,
       T
     > {}
+  interface AsymmetricMatchersContaining extends TestingLibraryMatchers {}
 }
