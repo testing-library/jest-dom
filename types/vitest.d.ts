3fcc061151e659afc19f2ef49ed1e8fbb83d9241
Mordechai Dror <vorant94@pm.me>
2024-09-12 15:00:05 +0300

    Revert "adjust vitest ambient declaration file according to vitest docs (#612)"
    This reverts commit 06d19ba759a5ff4b51417cc9a7ecb72c785eb74d.

--- types/vitest.d.ts
+++ types/vitest.d.ts
@@ -1,15 +1,11 @@
 import {type expect} from 'vitest'
 import {type TestingLibraryMatchers} from './matchers'
 
-declare module 'vitest' {
-  interface Assertion<T = any>
+export {}
+declare module '@vitest/expect' {
+  interface JestAssertion<T = any>
     extends TestingLibraryMatchers<
       ReturnType<typeof expect.stringContaining>,
       T
-    > {}
-  interface AsymmetricMatchersContaining
-    extends TestingLibraryMatchers<
-      ReturnType<typeof expect.stringContaining>,
-      any
     > {}
 }
