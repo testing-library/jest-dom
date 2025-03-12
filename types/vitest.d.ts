c0e63ec49de7b84db33058df71e52513d68cffe9
Dina <dnafication@users.noreply.github.com>
2025-03-12 17:58:49 +1100

    fix: import vitest is not needed to extend the module
    The import is causing an issue as it is now making vitest a dependency of the module and results in error when running tests such as:
    ```
    Error: Cannot find package 'vitest' imported from /path/to/project/ui/node_modules/.pnpm/@testing-library+jest-dom@6.6.3/node_modules/@testing-library/jest-dom/dist/vitest.mjs
    ```

--- types/vitest.d.ts
+++ types/vitest.d.ts
@@ -1,15 +1,7 @@
-import 'vitest'
 import {type TestingLibraryMatchers} from './matchers'
 
 declare module 'vitest' {
-  interface Assertion<T = any>
-    extends TestingLibraryMatchers<
-      any,
-      T
-    > {}
+  interface Assertion<T = any> extends TestingLibraryMatchers<any, T> {}
   interface AsymmetricMatchersContaining
-    extends TestingLibraryMatchers<
-      any,
-      any
-    > {}
+    extends TestingLibraryMatchers<any, any> {}
 }
